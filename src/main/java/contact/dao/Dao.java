d35f60e73a1b8c1283d8f33474b1d7f6ef7b9b90
vl0784 <todv@ukr.net>
2016-07-08 12:16:25 +0300

    Commit1

---
+++ src/main/java/contact/dao/Dao.java
@@ -0,0 +1,16 @@
+package contact.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface Dao<T, Id extends Serializable> {
+
+	public void create(T entity);
+
+	public List<T> read();
+
+	public void update(T entity);
+
+	public void delete(Integer id);
+
+}
