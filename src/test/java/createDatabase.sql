6c41e2fb50afae06443a5121893a20b39a659fba
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2016-10-12 15:49:45 +0300

    Commit 12.10.2016

---
+++ src/test/java/createDatabase.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS store (
+  Idstore  INT NOT NULL AUTO_INCREMENT,
+  city VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idstore));
+
+
+
+CREATE TABLE IF NOT EXISTS department (
+  iddepartment INT NOT NULL AUTO_INCREMENT   PRIMARY KEY,
+  name VARCHAR(45) NULL,
+  email VARCHAR(45) NULL,
+  phone VARCHAR(45) NULL,
+  post VARCHAR(45) NULL,
+  store_idstore INT NOT NULL,
+    FOREIGN KEY (store_idstore)   REFERENCES store (idstore));
